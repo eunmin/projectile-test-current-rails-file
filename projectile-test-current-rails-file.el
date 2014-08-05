d5c33868486be5fd600d689803939406c0407a49
Eunmin <telepopsound@gmail.com>
2014-08-06 00:11:54 +0900

    fix bug

--- projectile-test-current-rails-file.el
+++ projectile-test-current-rails-file.el
@@ -2,23 +2,24 @@
 
 (defun projectile-test-run (test-file-name)
   (interactive)
-  (message (concat "Test:" test-file-name))
   (if test-file-name
-      (setq last-test-file-name test-file-name))
-  (cond
-   ((member (projectile-project-type) '(rails-test ruby-test)) 
-    (compilation-start (concat projectile-ruby-test-cmd " TEST=" test-file-name)))
-   ((member (projectile-project-type) '(lein))
-    (compilation-start (concat projectile-lein-test-cmd " " test-file-name)))))
+      (progn (setq last-test-file-name test-file-name)
+       (message (concat "Test:" test-file-name))
+       (cond
+        ((member (projectile-project-type) '(rails-test ruby-test)) 
+         (compilation-start (concat projectile-ruby-test-cmd " TEST=" test-file-name)))
+        ((member (projectile-project-type) '(lein))
+         (compilation-start (concat projectile-lein-test-cmd " " test-file-name)))))
+    (message "No test file.")))
 
 (defun projectile-test-current-rails-file ()
   (interactive)
   (let* ((current-file-name (buffer-file-name (current-buffer)))
          (test-file-name (if (projectile-test-file-p current-file-name)
-                             current-file-name (concat (projectile-project-root) (projectile-find-matching-test current-file-name)))))
-    (if (projectile-find-matching-test current-file-name)
-        (projectile-test-run test-file-name)
-      (message "No test file."))))
+                             current-file-name
+                             (if (projectile-find-matching-test current-file-name)
+                                 (concat (projectile-project-root) (projectile-find-matching-test current-file-name))))))
+    (projectile-test-run test-file-name)))
 
 (defun projectile-test-last-test-file ()
   (interactive)
