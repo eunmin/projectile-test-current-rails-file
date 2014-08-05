522f990c2cfc93c4fa604e6ddc5832bb2f2cd75e
Todd <todd@kakao.com>
2014-08-05 11:13:16 +0900

    add projectile-test-last-test-file function

--- projectile-test-current-rails-file.el
+++ projectile-test-current-rails-file.el
@@ -1,7 +1,27 @@
+(setq last-test-file-name nil)
+
+(defun projectile-test-run (test-file-name)
+  (interactive)
+  (message (concat "Test:" test-file-name))
+  (if test-file-name
+      (setq last-test-file-name test-file-name))
+  (cond
+   ((member (projectile-project-type) '(rails-test ruby-test)) 
+    (compilation-start (concat projectile-ruby-test-cmd " TEST=" test-file-name)))
+   ((member (projectile-project-type) '(lein))
+    (compilation-start (concat projectile-lein-test-cmd " " test-file-name)))))
+
 (defun projectile-test-current-rails-file ()
   (interactive)
   (let* ((current-file-name (buffer-file-name (current-buffer)))
          (test-file-name (if (projectile-test-file-p current-file-name)
                              current-file-name (concat (projectile-project-root) (projectile-find-matching-test current-file-name)))))
-    (message (concat "Test:" test-file-name))
-    (compilation-start (concat projectile-ruby-test-cmd " TEST=" test-file-name))))
+    (if (projectile-find-matching-test current-file-name)
+        (projectile-test-run test-file-name)
+      (message "No test file."))))
+
+(defun projectile-test-last-test-file ()
+  (interactive)
+  (if last-test-file-name
+      (projectile-test-run last-test-file-name)
+    (message "No last test file.")))
